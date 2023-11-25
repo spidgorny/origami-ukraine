911ecbf8013c74711f560b892d277bc289fa06be
Slawa <spidgorny@gmail.com>
2023-11-26 00:11:24 +0100

    build: ls /var/task

--- src/app/api/ls/route.ts
+++ src/app/api/ls/route.ts
@@ -9,7 +9,11 @@ export function GET(request: Request) {
     filesVarTask = e?.message;
   }
   return Response.json({
+    cwd: process.cwd(),
     filesDot: fs.readdirSync("."),
+    filesDotNext: fs.readdirSync(".next"),
+    filesDotVercel: fs.readdirSync(".vercel"),
+    filesOutstatic: fs.readdirSync("outstatic"),
     filesVarTask: filesVarTask,
   });
 }
