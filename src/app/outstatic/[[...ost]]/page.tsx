d7ccef4f8b2d93f950c8c7527c1473f2c862dad6
Slawa <spidgorny@gmail.com>
2023-11-25 23:35:02 +0100

    build: downgrade next

--- src/app/outstatic/[[...ost]]/page.tsx
+++ src/app/outstatic/[[...ost]]/page.tsx
@@ -2,7 +2,7 @@ import 'outstatic/outstatic.css'
 import { Outstatic } from 'outstatic'
 import { OstClient } from 'outstatic/client'
 
-export default async function Page({ params }: { params: { ost: string[] } }) {
-  const ostData = await Outstatic()
-  return <OstClient ostData={ostData} params={params} />
+export default async function Page({ params }: Readonly<{ params: { ost: string[] } }>) {
+  const ostData = await Outstatic();
+  return <OstClient ostData={ostData} params={params} />;
 }
