492a6bcf12dcee2b806c8f767b29886e97505326
Slawa <spidgorny@gmail.com>
2023-11-25 22:19:08 +0100

    fix: /outstatic

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,7 +7,6 @@ let locales = ["en", "uk"];
 // Get the preferred locale, similar to the above or using a library
 function getLocale(request: NextRequest) {
   let headers = Object.fromEntries(request.headers.entries());
-  console.log({ headers });
   let languages = new Negotiator({ headers }).languages();
   let defaultLocale = "en";
 
@@ -23,14 +22,20 @@ export function middleware(request: NextRequest) {
 
   if (pathnameHasLocale) return;
 
-  const locale = getLocale(request);
-
+  const firstLevel = request.nextUrl.pathname.split("/").filter(Boolean)[0];
+  console.log({
+    pathname: request.nextUrl.pathname,
+    firstLevel,
+  });
   const blacklist = ["outstatic"];
-  if (blacklist.includes(locale)) {
+  if (blacklist.includes(firstLevel)) {
     return;
   }
 
   // Redirect if there is no locale
+  const locale = getLocale(request);
+  // console.log({ locale });
+
   request.nextUrl.pathname = `/${locale}${pathname}`;
   // e.g. incoming request is /products
   // The new URL is now /en-US/products
